6a499bb21d781a0d681ca9be6e8b579f086029e8
kabhay84 <Work@tHome2020>
2020-11-19 02:39:57 +0000

    reset variable

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+
     private String welcomeMessage;
 
     public WelcomeController(
